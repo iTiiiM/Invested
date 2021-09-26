f2aefda2aa7d443fef04440578f0757385a5883d
MEW#IT14 <mewloft@gmail.com>
2021-09-26 14:19:55 +0700

    initial commit
    Signed-off-by: MEW#IT14 <mewloft@gmail.com>

---
+++ Invested/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Invested
+//
+//  Created by Piyatat  Thianboonsong on 26/9/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Invested/InvestedApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestedApp.swift
+//  Invested
+//
+//  Created by Piyatat  Thianboonsong on 26/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct InvestedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
